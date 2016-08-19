27561d99d64b1a6fcc918398c5f9c9904f86b42e
Christian Pohl <cpo@blubberlutsch.munich.secaron.de>
2016-08-19 14:06:02 +0200

    farbfunktion tasten

--- mywordclock/mywordclock.ino
+++ mywordclock/mywordclock.ino
@@ -1,3 +1,12 @@
+//
+// mywordclock
+// Autor C. Pohl
+// http://github.com/zappzarazupp
+//
+// Mein version einer Wortuhr
+// Baut auf WS2811B LED
+// 
+
 #include <Adafruit_NeoPixel.h>
 #include <Time.h>
 #include <DCF77.h>
@@ -5,19 +14,22 @@
 //#define DEBUG ja
 #undef DEBUG
 
+// PINs
 #define PIN_MLED  6
 #define PIN_ZLED  7
 #define PIN_LDR   A0  // Analog
 #define PIN_DCF   2
 #define DCF_INTERRUPT 0  // Interrupt number associated with PIN_DCF
+#define PIN_TASTCOLM  3  // farbmodus 
+#define PIN_TASTDISP  4  // anzeigemodus
 
+// einige Konstanten
 #define M_WIDTH 11
 #define M_HEIGHT 10
 #define Z_LEDS 5
-
-#define MINBRIGHT 100.0
+#define TPOLL=10;
+#define MINBRIGHT 50.0
 #define MAXBRIGHT 255.0
-
 
 // texte
 #define T_ES          line[0] |= 0b1100000000000000
@@ -59,6 +71,7 @@
 #define T_M4  0b11011000
 #define T_ST  0b00100000
 
+// Globale Variablen
 Adafruit_NeoPixel m_led = Adafruit_NeoPixel(M_WIDTH * M_HEIGHT, PIN_MLED, NEO_GRB + NEO_KHZ800);
 Adafruit_NeoPixel z_led = Adafruit_NeoPixel(Z_LEDS, PIN_ZLED, NEO_GRB + NEO_KHZ800);
 
@@ -70,21 +83,35 @@ uint8_t cursec=0, oldsec=0;
 uint8_t curzled=0, oldzled=0;
 uint16_t curbright=0, oldbright=0;
 uint32_t curstatus_color=0, oldstatus_color=0;
+uint8_t colormode=0;
+uint8_t dispmode=0;
+uint8_t pollcolm=0;
+uint8_t tastcolm=0;
+uint8_t polldisp=0;
+uint8_t tastdisp=0;
 
 time_t time;
 DCF77 DCF = DCF77(PIN_DCF,DCF_INTERRUPT);
 
+/////////////////////////////////////
+// SETUP
 void setup() {
   
-  #ifdef DEBUG
-  Serial.begin(9600);  //Begin serial communcation
-  #endif
+#ifdef DEBUG
+Serial.begin(9600);  //Begin serial communcation
+#endif
+
+  pinMode(PIN_TASTCOLM, INPUT);
+  pinMode(PIN_TASTDISP, INPUT);
+ 
+  randomeSeed(analogRead(PIN_LDR));
 
   m_led.begin();
   m_led.show(); // Initialize all pixels to 'off'
   z_led.begin();
   z_led.show(); // Initialize all pixels to 'off'
 
+  //Test
   colorWipe(&m_led,m_led.Color(255, 255, 255), 10); // white
   colorWipe(&m_led,m_led.Color(0, 0, 0), 10); // off 
   colorWipe(&z_led,z_led.Color(255, 255, 255), 50); // white
@@ -95,75 +122,97 @@ void setup() {
   setZLED();
   z_led.show();
 
-
   setTime(20,07,0,18,8,2016);
   //setTime(hr,min,sec,day,month,yr);
-/*  DCF.Start();
+  DCF.Start();
   setSyncInterval(30);
   setSyncProvider(DCF.getTime);
-
-  // wait until the time is set by the sync provider      
-  while(timeStatus() == timeNotSet) {  
-    if(digitalRead(PIN_DCF) == 1) {
-      curstatus_color = z_led.Color(255,0,255);
-    }
-    else {
-      curstatus_color = z_led.Color(255,0,0);
-    }
-    if(curstatus_color != oldstatus_color) {
-      oldstatus_color=curstatus_color;
-      setZLED();
-      z_led.show();
-    }
-  } 
-  oldstatus_color=curstatus_color=z_led.Color(0,255,0);
-  setZLED();
-  z_led.show();*/
 }
 
+/////////////////////////////////////
+// LOOP
 void loop() {
 
-  int16_t diff=0;
-  //Test
-  delay(100);
-
-  curbright = (int)(MINBRIGHT+(MAXBRIGHT-MINBRIGHT)*analogRead(PIN_LDR)/1023.0);
-  diff = oldbright - curbright;
-  if(diff<0) { diff = diff * -1; }
 #ifdef DEBUG
-Serial.print(oldbright);
-Serial.print(" : ");
-Serial.print(curbright);
-Serial.print(" : ");
-Serial.println(diff);
+delay(100);
 #endif
 
+  int16_t diff=0;
+ 
+  // meine version, das prellen in sw abzufangen #define TPOLL zum optimieren
+  if(digitalRead(PIN_TASTCOLM == HIGH) {
+    pollcolm+=1;
+    if(pollcolm >= TPOLL && tastcolm==0) {
+      pollcolm=TPOLL;
+      tastcolm=1;
+      //tastenaktion drück   
+      colmode+=1;
+      colmode%=2; // gibt momentan nur 2 colmode
+      oldbright=0; // erzwinge neuzeichnen
+    }
+  }
+  else {
+    pollcolm-=1;
+    if(pollcolm <= 0 && tastcolm==1) {
+      pollcolm=0;
+      tastcolm=0;
+      //tastenaktion loslass 
+    }
+  }
+
+//  if(digitalRead(PIN_TASTDISP == HIGH) {
+//    polldisp+=1;
+//    if(polldisp >= TPOLL && tastdisp==0) {
+//      polldisp=TPOLL;
+//      tastdisp=1;
+//      //tastenaktion drück   
+//    }
+//  }
+//  else {
+//    polldisp-=1;
+//    if(polldisp <= 0 && tastdisp==1) {
+//      polldisp=0;
+//      tastdisp=0;
+//      //tastenaktion loslass 
+//    }
+//  }
+//
+
+  // Helligkeit einlesen
+  curbright = (int)(MINBRIGHT+(MAXBRIGHT-MINBRIGHT)*analogRead(PIN_LDR)/1023.0);
+  diff = oldbright - curbright;
+  if(diff<0) { diff = -diff;  }  //abs macht probleme
+
+  // Zeit lesen
   time=now();
   curmin=minute(time);
   curstd=hourFormat12(time);
   cursec=second(time);
-/*
-  if(timeStatus() == timeNotSet) {  
-    if(digitalRead(PIN_DCF) == 1) {
-      curstatus_color = z_led.Color(255,0,255);
-    }
-    else {
-      curstatus_color = z_led.Color(255,0,0);
-    }
-  } 
-  else {
-    if(digitalRead(PIN_DCF) == 1) {
-      curstatus_color = z_led.Color(0,255,255);
-    }
-    else {
-      curstatus_color = z_led.Color(0,255,0);
-    }
-  } 
-*/
+
+//  if(timeStatus() == timeNotSet) {  
+//    if(digitalRead(PIN_DCF) == 1) {
+//      curstatus_color = z_led.Color(255,0,255);
+//    }
+//    else {
+//      curstatus_color = z_led.Color(255,0,0);
+//    }
+//  } 
+//  else {
+//    if(digitalRead(PIN_DCF) == 1) {
+//      curstatus_color = z_led.Color(0,255,255);
+//    }
+//    else {
+//      curstatus_color = z_led.Color(0,255,0);
+//    }
+//  } 
+//
   
+  // sekundenblinken statusled
+  // anpassen, wenn DCF Blinken dazukommt
   curzled = (curzled & T_M4) | ((cursec % 2) * T_ST);
   
-  if(curstd != oldstd || curmin != oldmin) {  // eigentlich nur alle %5 minuten; nicht neuzeichnen bei leichten helligkeitsschwankungen
+  // Alle 5 minuten Matrix neu schreiben
+  if(curmin != oldmin && curmin%5 == 0) {
     setText();
     setMin();
     setMatrixLED();
@@ -171,7 +220,8 @@ Serial.println(diff);
     oldstd=curstd;
     oldmin=curmin;
     m_led.show();
-  } else if (diff > 10) {
+  }
+  else if (diff > 10) { // bei größeren helligkeitsschwakungen neu zeichnen mit neuer helligkeit
 #ifdef DEBUG
 Serial.print(oldbright);
 Serial.print(" m ");
@@ -182,6 +232,8 @@ Serial.println(diff);
     m_led.setBrightness(curbright);
     m_led.show();
   }
+
+  // wenn sich an den zled was geändert hat
   if(curzled != oldzled || curstatus_color != oldstatus_color) {
     setZLED();
     z_led.setBrightness(curbright);
@@ -189,7 +241,8 @@ Serial.println(diff);
     oldbright=curbright;
     oldstatus_color=curstatus_color;
     z_led.show();
-  } else if (diff > 10) {
+  }
+  else if (diff > 10) { // bei größeren helligkeitsschwakungen neu zeichnen mit neuer helligkeit
 #ifdef DEBUG
 Serial.print(oldbright);
 Serial.print(" z ");
@@ -204,7 +257,8 @@ Serial.println(diff);
 
 }
 
-// Get number of LED in matrix
+/////////////////////////////////////
+// LED nummer aus matrixkoordinaten
 uint16_t xy( uint8_t x, uint8_t y)
 {
   uint16_t i;
@@ -213,6 +267,8 @@ uint16_t xy( uint8_t x, uint8_t y)
   return i;
 }
 
+/////////////////////////////////////
+// Matrix LED setzen aus array 'line[]'
 void setMatrixLED() {
   uint32_t c;
   uint16_t x;
@@ -220,8 +276,7 @@ void setMatrixLED() {
   for(int i=0; i<=9; i++) { // zeile
     for(int j=0; j<=15; j++) { // spalte
       x=bitRead(line[i],15-j);
-      c=ctimeOfDay(&m_led);
-      //c=m_led.Color(255, 255, 255); // ersetzen mit funktion für farbmodus
+      c=mcolor(j,i);
       if(x == 1) {
         m_led.setPixelColor(xy(j,i),c);
       }
@@ -233,6 +288,9 @@ void setMatrixLED() {
   }
 }
 
+/////////////////////////////////////
+// Texte aus aktueller zeit setzen
+// bei mehr schreibweisen entscheidet der zufall
 void setText() {
 
   T_ES;
@@ -333,6 +391,9 @@ void setText() {
   }
 }
 
+/////////////////////////////////////
+// Stundentext
+// sonderfall: es ist ein uhr
 void setHourText(uint8_t h) {
   switch(h) {
     case 0:
@@ -378,6 +439,9 @@ void setHourText(uint8_t h) {
   }
 }
 
+/////////////////////////////////////
+// zLED aus curzled setzen
+// sonderbehandlung status LED
 void setZLED() {
   uint8_t x=0;
   uint32_t c=0;
@@ -387,8 +451,7 @@ void setZLED() {
       c=curstatus_color;
     }
     else {
-      c=ctimeOfDay(&z_led);
-      //c=z_led.Color(255, 255, 255); // ersetzen mit funktion für farbmodus
+      c=zcolor(i>2?i-=1:i); // zminuten sind index 0,1,3,4 aber für farbe muss es 0,1,2,3 sein
     }
     if(x == 1) {
       z_led.setPixelColor(i,c);
@@ -399,6 +462,8 @@ void setZLED() {
   }
 }
 
+/////////////////////////////////////
+// zwischenminuten setzen
 void setMin() {
   curzled=curzled & T_ST; //status behalten, rest zurücksetzen
   switch(curmin % 5) {
@@ -417,6 +482,55 @@ void setMin() {
   }
 }
 
+/////////////////////////////////////
+// farbfunktion(en)
+uint32_t mcolor(uint8_t x, uint8_t y) { // farbfunktion für Matrix
+  switch(colormode) {
+    case 0:
+    default:
+      return colorwheel(m_led,110,xy(x,y)); 
+    case 1:
+      int iOfDay=(int)((curstd*60.0+curmin)/5.0); // aktuelle anzahl der 5 min intervalle
+      return colorwheel(m_led,288,iOfDay); // ein tag hat 288 5min intervalle
+  }
+}
+
+uint32_t zcolor(uint8_t i) { // farbfunktion für minuten
+  switch(colormode) {
+    case 0:
+    default:
+      return colorwheel(z_led,4,i); 
+    case 1:
+      int iOfDay=(int)((curstd*60.0+curmin)/5.0); // aktuelle anzahl der 5 min intervalle
+      return colorwheel(z_led,288,iOfDay); // ein tag hat 288 5min intervalle
+  }
+}
+
+// farbrad
+// 255,0,0 --> 0,255,0
+// 0,255,0 --> 0,0,255
+// 0,0,255 --> 255,0,0
+uint32_t colorwheel(Adafruit_NeoPixel *strip, uint8_t wheelsteps, uint8_t curstep) {
+
+  float p=wheelsteps/3.0;
+  float s=255.0/p); // schrittweite
+  
+  // 255,0,0 --> 0,255,0
+  if(curstp < p) {
+    return strip->Color(255-curstp*s, curstp*s, 0);
+  }
+  // 0,255,0 --> 0,0,255
+  if(curstp < 2*p) {
+    curstp -= p;
+    return strip->Color(0, 255-curstp*s, curstp*s);
+  }
+  // 0,0,255 --> 255,0,0
+    curstp -= 2*p;
+    return strip->Color(curstp*s,0, 255-curstp*s);
+}
+
+/////////////////////////////////////
+// Test
 // Fill the dots one after the other with a color
 void colorWipe(Adafruit_NeoPixel *strip,uint32_t c, uint8_t wait) {
   for(uint16_t i=0; i<strip->numPixels(); i++) {
@@ -424,27 +538,6 @@ void colorWipe(Adafruit_NeoPixel *strip,uint32_t c, uint8_t wait) {
     strip->show();
     delay(wait);
   }
-}
-
-// ein tag hat 288 5min intervalle
-// 96 + 96 + 96
-// 0,255,0 --> 0,0,255  schrittweite 2,65625
-// 0,0,255 --> 255,0,0
-// 255,0,0 --> 0,255,0
-uint32_t ctimeOfDay(Adafruit_NeoPixel *strip) {
-
-  int iOfDay=hour()*60+minute()/5;
-  float s=2.65625;
-  
-  if(iOfDay < 96) {
-    return strip->Color(255 - iOfDay * s, 0, iOfDay * s);
-  }
-  if(iOfDay < 2*96) {
-    iOfDay -= 96;
-    return strip->Color(0, iOfDay * s, 255 - iOfDay * s);
-  }
-  iOfDay -= 2*96;
-  return strip->Color(iOfDay * s, 255 - iOfDay * s, 0);
 }
 
 
